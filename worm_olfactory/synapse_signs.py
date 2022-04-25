0f2503b751b7a3127b207deb93ea3d85011cddef
Lu Mi <mil@turagas-ws2.int.janelia.org>
2022-04-24 22:15:51 -0400

    delete old model, data

--- worm_olfactory/synapse_signs.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-import torch
-
-class Synapse_Signs(nn.Module):
-    def __init__(self, N_neurons, random_seed = None):
-        super(Synapse_Signs, self).__init__()
-        torch.random.manual_seed(random_seed)
-        self.synapse_signs = torch.nn.Parameter(torch.rand((N_neurons, N_neurons), dtype = torch.float32, device = 'cuda')-0.2)
